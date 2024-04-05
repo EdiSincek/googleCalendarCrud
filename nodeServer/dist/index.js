712c552a0d41e872555c72d650993e1d29c70bd9
Edi Sincek <edi.sincek@gmail.com>
2024-04-05 14:40:51 +0200

    Adding express

--- nodeServer/dist/index.js
+++ nodeServer/dist/index.js
@@ -1,2 +1,10 @@
-export {};
+import express from "express";
+const app = express();
+const PORT = 3000; //Will change it to env later
+app.listen(PORT, () => {
+  console.log("Server is running at port: ", PORT);
+});
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 //# sourceMappingURL=index.js.map
