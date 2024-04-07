d83a9b0acea391098ffb9278949f3d7ebf3f8ccd
Edi Sincek <edi.sincek@gmail.com>
2024-04-07 14:06:16 +0200

    Upcoming events component initial

--- nodeServer/src/index.ts
+++ nodeServer/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import googleRouter from "./routes/googleRoutes.js";
+import cors from "cors";
 
 const app: Express = express();
 const PORT: number = 3001;
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log("Server is running at port: ", PORT);
